6d2dbf25a7cfc634f219cc211c6868aa4718aef3
aashishrbhandari <asharry97.bhandari@gmail.com>
2021-11-28 16:58:26 +0530

    removed node_modules

---
+++ simple-starter-react-app/app/Main.js
@@ -0,0 +1,32 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+// My Components
+import Header from "./components/Header"
+import HomeGuest from "./components/HomeGuest"
+import Footer from "./components/Footer"
+import About from "./components/About"
+import Terms from "./components/Terms"
+
+function Main() {
+	return (
+		<>
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route path="/" element={<HomeGuest />}></Route>
+					<Route path="/about" element={<About />}></Route>
+					<Route path="/terms" element={<Terms />}></Route>
+				</Routes>
+				<Footer />
+			</BrowserRouter>
+		</>
+	)
+}
+
+ReactDOM.render(<Main />, document.querySelector("#app"))
+
+if (module.hot) {
+	module.hot.accept()
+}
